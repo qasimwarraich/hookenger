476b6ffb23da4b9dd1df4143d79e4c9ef6201cbe
qasimwarraich <qasim.warraich@gmail.com>
2022-02-17 18:10:36 +0100

    Some refactoring and formatting

--- src/index.js
+++ src/index.js
@@ -2,40 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { sendNotice } from './sendnotice.js';
 
-dotenv.config({path: "../.env"})
+dotenv.config({ path: '../.env' });
 
 const app = express();
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('<p> 🤠 lol <p>');
-})
-console.log(process.cwd());
+    res.status(200).send(`<p> Hey 🤠, You shouldn't be here. <p>`);
+});
 
 app.post('/hook', (req, res) => {
-    console.log(req.body)
-    res.status(200).send("spam")
-})
+    if (req.body.message) {
+        res.status(200).send();
+    } else {
+        res.status(420);
+    }
+    sendNotice(req.body.message);
+});
 
-app.listen(process.env.PORT, () => console.log(`App is live on http://localhost:6969`));
-
-const client = matrix.createClient({
-    baseUrl: process.env.MATRIX_URL,
-    accessToken: process.env.USER_ACCESS_TOKEN,
-    userID: process.env.USER_ID
-})
-
-
-sendNotice("🤠 Hello Spam 🤠");
-
-function sendNotice(body) {
-    var roomID = process.env.ROOM_ID;
-    var content = {
-        "body": body.substring(0),
-        "msgtype": "m.notice"
-    };
-    client.sendEvent(roomID, "m.room.message", content, "", (err, res) => {
-        console.log(err);
-    });
-}
+app.listen(process.env.PORT, () =>
+    console.log(`App is live on http://localhost:${process.env.PORT}`)
+);
