5a5bc8f7ab28bddd1ed45397277bca3934c33e73
qasimwarraich <qasim.warraich@gmail.com>
2022-02-17 15:40:11 +0100

    Add a post endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
-
+import bodyParser from "body-parser";
 
 dotenv.config({path: "../.env"})
 
@@ -13,5 +13,9 @@ app.get('/', (req, res) => {
 })
 console.log(process.cwd());
 
+app.post('/hook', (req, res) => {
+    console.log(req.body)
+    res.status(200).send("spam")
+})
 
 app.listen(process.env.PORT, () => console.log(`App is live on http://localhost:6969`));
