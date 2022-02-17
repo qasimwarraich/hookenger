149c9ec7c3037935303093b2f89ff0f2d336c48c
qasimwarraich <qasim.warraich@gmail.com>
2022-02-17 18:09:52 +0100

    Updated imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import dotenv from "dotenv"
-import bodyParser from "body-parser";
+import express from 'express';
+import dotenv from 'dotenv';
+import { sendNotice } from './sendnotice.js';
 
 dotenv.config({path: "../.env"})
 
